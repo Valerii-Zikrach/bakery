83e4ea46e03ef2d0e2b9b24951bb692510f9f405
Valerii Zikrach <valera.zikrach@gmail.com>
2020-06-22 22:20:51 +0300

    added bread-line section

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,35 @@
 $(function () {
-
+	$('.bread-line__slider').slick({
+		dots: true,
+		infinite: true,
+		// autoplay: true,
+		autoplaySpeed: 2000,
+		slidesToShow: 4,
+		slidesToScroll: 3
+		// responsive: [
+		//   {
+		// 	 breakpoint: 1024,
+		// 	 settings: {
+		// 		slidesToShow: 3,
+		// 		slidesToScroll: 3,
+		// 		infinite: true,
+		// 		dots: true
+		// 	 }
+		//   },
+		//   {
+		// 	 breakpoint: 600,
+		// 	 settings: {
+		// 		slidesToShow: 2,
+		// 		slidesToScroll: 2
+		// 	 }
+		//   },
+		//   {
+		// 	 breakpoint: 480,
+		// 	 settings: {
+		// 		slidesToShow: 1,
+		// 		slidesToScroll: 1
+		// 	 }
+		//   }
+		// ]
+	});
 });
